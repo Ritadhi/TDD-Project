6cb8b0659af0de91b066a365f36e123b62056afe
ritadhi <ritadhiroy1998@gmail.com>
2025-02-12 16:49:01 +0530

    feat: added code to accept new delimiter

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,13 @@
 const add = function (str) {
     if(str.length === 0) return 0;
+    if(str.startsWith('//')) {
+        str = str.split('\n');
+        const delimeter = str.shift().replace(/\/\//g, '');
+        str = str.join('\n');
+        return str.split(',').map(el => el.split('\n')).flat().map(el => el.split(delimeter)).flat().reduce((acc, curr) => {
+            return acc + Number(curr);
+        }, 0)
+    }
     return str.split(',').map(el => el.split('\n')).flat().reduce((acc, curr) => {
         return acc + Number(curr);
     }, 0)
