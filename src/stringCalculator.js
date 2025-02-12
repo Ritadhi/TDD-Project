ff102413588a4aef8d5107ed038c13f79eeda3e9
ritadhi <ritadhiroy1998@gmail.com>
2025-02-12 18:50:56 +0530

    feat: updated logic for multiple long delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,18 +1,19 @@
 const add = function (str) {
     if(str.length === 0) return 0;
-    const delimeters = [',', '\n'];
+    const delimeters = ['\n'];
     if(str.startsWith('//')) {
         str = str.split('\n');
-        const delimeter = str.shift().replace(/\/\//g, '').replace(/[\[\]]/g, '');
-        delimeters.push(delimeter)
+        const delimeter = str.shift().replace(/\/\//g, '').split('][').map(el => el.replace(/[\[\]]/g, ''));
+        delimeters.push(...delimeter)
         str = str.join('\n');
     }
-    let regex = new RegExp('[' + delimeters.join('') + ']+', 'g');
-    const numberArr = str.split(regex);
+    delimeters.forEach(del => str = str.replaceAll(del, ','));
+    const numberArr = str.split(',');
     const negativeNumbers = numberArr.filter(el => el < 0);
     return negativeNumbers.length > 0 
         ? `negative numbers not allowed ${negativeNumbers.join(',')}` 
         : numberArr.reduce((acc, curr) => {
+            if(isNaN(Number(curr))) return NaN
             return acc + (curr < 1000 ? Number(curr) : 0);
         }, 0);
 }
